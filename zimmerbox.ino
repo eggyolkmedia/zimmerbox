55ebae0fd41df27f33fc756dc080edb49703cbab
eggyolkmedia <github@eggyolkmedia.com>
2017-08-11 17:26:43 +0300

    Adding logger and wifi manager, param config

--- zimmerbox.ino
+++ zimmerbox.ino
@@ -1,192 +1,222 @@
+#include <ArduinoLog.h>
 #include <Bounce2.h>
+#include <DNSServer.h>
 #include <EEPROM.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
-// TODO Implement basic config validation using this as a CRC
-#include "MD5Builder.h"
 #include <PubSubClient.h>
+#include <WiFiManager.h>
 
 #include "settings.h"
 
 #define MODE_NORMAL 0
 #define MODE_CONFIG 1
 
+// Uncomment once deployed
+// #define DISABLE_LOGGING
+#define LOG_LEVEL LOG_LEVEL_VERBOSE
+
 struct Config {
-  char wifi_ssid[20];
-  char wifi_pass[20];
+  char mqttHost[15];
+  char mqttPort[6];
 
-  char mqtt_host[15];
-  int mqtt_port;
-
-  char topic_state[64];
-  char topic_set[64];
+  char topicState[64];
+  char topicSet[64];
 
   // Intentionally placed at the end, so adding config fields will be detected as a change
   uint8_t hashcode[16];
 };
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-Config config;
+WiFiClient _espClient;
+PubSubClient _mqttClient(_espClient);
+Config _config;
 
-unsigned long last_state = 0;
-unsigned long last_button = 0;
-char msg[50];
-Bounce bounce;
+unsigned long _lastState = 0;
+unsigned long _lastButton = 0;
+Bounce _bounce;
 
 // TODO support more output types (dimmables, etc.)
-bool relay_state = LOW;
-// byte mode;
+bool _relayState = LOW;
+
 
 void setup() {
   Serial.begin(9600);
-  delay(1000); // TODO Nonblocking wait (maybe blink led)
-   
-  EEPROM.begin(sizeof(Config));
-  load_config();
+  while(!Serial && !Serial.available()){}   
 
-  // TODO allow user config changes; currently just init config to default if not valid
-  if (!validate_config()) {
-    config = initial_config();
-    save_config();
-  }
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
   
-  // Determine mode according to config validity
-  //mode = validate_config() ? MODE_NORMAL : MODE_CONFIG;
+  EEPROM.begin(sizeof(Config));
+  loadConfig();
 
-  bounce.attach(PIN_BUTTON);
-  bounce.interval(50);
+  _bounce.attach(PIN_BUTTON);
+  _bounce.interval(50);
   pinMode(PIN_RELAY, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
 
-  setup_wifi();
-  client.setServer(config.mqtt_host, config.mqtt_port);
-  client.setCallback(callback);
+  setupWifi();
+
+  _mqttClient.setServer(_config.mqttHost, atoi(_config.mqttPort));
+  _mqttClient.setCallback(messageReceivedCallback);
 }
 
-bool validate_config() {
+bool isValidConfig() {
   uint8_t buff[16];
-  get_config_hashcode(buff);
-  return memcmp(config.hashcode, buff, 16) == 0; 
+  getConfigHashcode(buff);
+  return memcmp(_config.hashcode, buff, 16) == 0; 
 }
 
 // TODO change signature to return hash rather than populate the input buffer
-void get_config_hashcode(uint8_t* buff16) {
+void getConfigHashcode(uint8_t* buff16) {
   MD5Builder md5;
   md5.begin();
-  md5.add((uint8_t*)&config, sizeof(Config)-16); // Exclude previous hashcode
+  md5.add((uint8_t*)&_config, sizeof(Config)-16); // Exclude previous hashcode
   md5.calculate();
   md5.getBytes(buff16);    
 }
 
-void save_config() {
-  EEPROM.put(0, config);
+void saveConfig() {
+  getConfigHashcode(_config.hashcode);
+  EEPROM.put(0, _config);
   EEPROM.commit();
 }
 
-void load_config() {
-  // Validate config (maybe store checksum? if missing, go into config mode; check that topic_set != topic_state)
-  EEPROM.get(0, config);  
-  Serial.print("Loaded config; ssid = ");
-  Serial.println(config.wifi_ssid);
-  Serial.print("Config valid? ");
-  Serial.println(validate_config());
+void loadConfig() {
+  // Validate config (maybe store checksum? if missing, go into config mode; check that topicSet != topicState)
+  EEPROM.get(0, _config);   
+  Log.trace("Loaded config; valid? %T\n", isValidConfig());
 }
 
-void setup_wifi() {
-  // TODO Replace with optional logging
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(config.wifi_ssid);
+void setupWifi() {
+  WiFiManager wifiManager;
 
-  WiFi.begin(config.wifi_ssid, config.wifi_pass);
+  // TODO Read default values from config if valid
+  bool validConfig = isValidConfig();
 
-  // TODO Replace with nonblocking connection + button check
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // TODO Consider adding labels
+  WiFiManagerParameter label_separator("<hr><br><div align='center'>ZimmerBox Config</div><br>");
+  WiFiManagerParameter label_mqtt_host("<label>MQTT Host</label>");
+  WiFiManagerParameter label_mqtt_port("<label>MQTT Port</label>");
+  WiFiManagerParameter label_topic_set("<label>SET Topic</label>");
+  WiFiManagerParameter label_topic_state("<label>STATE Topic</label>");
+  
+  WiFiManagerParameter paramMqttHost("mqtt_host", "", validConfig ? _config.mqttHost : "192.168.0.12", 20);
+  WiFiManagerParameter paramMqttPort("mqtt_port", "", validConfig ? _config.mqttPort : "1883", 10);
+  
+  // TODO Merge pubsub topics and move action to message
+  // TODO Consider defaulting to hardcoded topics, change only device ID
+  WiFiManagerParameter paramTopicSet("topic_set", "", validConfig ? _config.topicSet : "/zimmer/$name/set", 40);
+  WiFiManagerParameter paramTopicState("topic_state", "", validConfig ? _config.topicState : "/zimmer/$name/state", 40);
+
+  wifiManager.addParameter(&label_separator);
+  wifiManager.addParameter(&label_mqtt_host);
+  wifiManager.addParameter(&paramMqttHost);
+  wifiManager.addParameter(&label_mqtt_port);
+  wifiManager.addParameter(&paramMqttPort);
+  wifiManager.addParameter(&label_topic_set);
+  wifiManager.addParameter(&paramTopicSet);
+  wifiManager.addParameter(&label_topic_state);
+  wifiManager.addParameter(&paramTopicState);
+  
+  if (validConfig) {
+    // TODO Set timeout, so if AC fails due to network outage reconnect without dialog
+    wifiManager.autoConnect();
+  } else {
+    // TODO Necessary?
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+    // TODO Set specific / random AP name
+    // TODO Handle failures
+    wifiManager.startConfigPortal("ZimmerBox");
+
+    // Save
+    strcpy(_config.mqttHost, paramMqttHost.getValue());
+    strcpy(_config.mqttPort, paramMqttPort.getValue());
+    strcpy(_config.topicSet, paramTopicSet.getValue());
+    strcpy(_config.topicState, paramTopicState.getValue());
+
+    saveConfig();
+
+
+    // TODO REMOVE
+    Log.notice("Saved Config. Valid? %T\n", isValidConfig());
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Log.trace("WiFi connected [%s]\n", WiFi.localIP().toString().c_str());
 
+  // TODO Consider moving these somewhere else -- need to be done when wifi is connected but not related to wifi setup
   long now = millis();
-  last_state = now;
-  last_button = now;
-
-  update_relay();
+  _lastState = now;
+  _lastButton = now;
+  updateRelay();  
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // TODO Nicer logging
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+void saveConfigCallback() {
+  // Do nothing?
+}
+
+void messageReceivedCallback(char* topic, byte* payload, unsigned int length) {
+  Log.notice("Message received [%s] \n", topic, payload);
+  // TODO Check if works....
+//  char[length+1] msg;
+//  for (int i = 0; i < length; i++) {
+//    msg[i] = Serial.print((char)payload[i]);
+//  }
+//  Serial.println();
 
   // TODO More meaningful messages
-  relay_state = (char)payload[0] == '1';
-  update_relay();
+  _relayState = (char)payload[0] == '1';
+  updateRelay();
 }
 
 // TODO Replace with nonblocking reconnection
 // TODO Allow switch operation even when not connected to MQTT server
-void reconnect() {
+void mqttReconnect() {
   // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+  while (!_mqttClient.connected()) {
+    Log.trace("Attempting MQTT connection...\n");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) { // Todo ??
-      Serial.println("connected");      
-      client.subscribe(config.topic_set);
+    if (_mqttClient.connect("ESP8266Client")) { // Todo ??
+      Log.trace("connected\n");      
+      _mqttClient.subscribe(_config.topicSet);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      Log.trace("failed, rc = %d; will try again in 5 seconds\n", _mqttClient.state());      
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
-  update_state();
+  updateState();
 }
 
-void update_relay() {
+void updateRelay() {
   // TODO Insert relay debouncing
-  digitalWrite(PIN_RELAY, relay_state);
-  update_state();
+  digitalWrite(PIN_RELAY, _relayState);
+  updateState();
 }
 
-void update_state() {
-    last_state = millis();
+void updateState() {
+    _lastState = millis();
     //snprintf (msg, 50, "hello world #%ld", value);
-    msg[0] = relay_state ? '1' : '0';
+    char msg[2];
+    msg[0] = _relayState ? '1' : '0';
     msg[1] = 0;
-    Serial.print("[");
-    Serial.print(config.topic_state);
-    Serial.print("] published: ");
-    Serial.println(msg);
-    client.publish(config.topic_state, msg);
+    Log.notice("[%s] published: %s\n", _config.topicState, msg);    
+    _mqttClient.publish(_config.topicState, msg);
 }
 
-void check_button() {
-  bounce.update();
+void checkButton() {
+  _bounce.update();
   
-  if (bounce.fell()) {
-    last_button = millis();
-    relay_state = !relay_state;
-    update_relay();
+  if (_bounce.fell()) {
+    _lastButton = millis();
+    _relayState = !_relayState;
+    updateRelay();
 
     // TODO Start blinking after a while if button is pressed
   }
 
-  if (bounce.rose()) {
-    unsigned long diff = millis() - last_button;
+  if (_bounce.rose()) {
+    unsigned long diff = millis() - _lastButton;
     if (diff > BUTTON_LONG_PRESS_MILLIS) {
-      reset_box();
+      resetBox();
     }
   }
 }
@@ -194,34 +224,26 @@ void check_button() {
 // Note: first reset after a serial flash will get stuck on 'boot mode:(1,7)'.
 // This is a known ESP issue and affects only the first reset
 // See also: https://github.com/esp8266/Arduino/issues/1017
-void reset_box() {
-  Serial.println("Resetting... the module will start in config mode");
+void resetBox() {
+  Log.notice("Resetting... the module will start in config mode\n");
   // TODO uncomment once config mode is ready
   // memset(config.hashcode, ?, 16); // Wipe hashcode, to go into config mode after reboot
   // TODO remove this once config mode is ready
 
-  save_config();
+  saveConfig();
   ESP.restart();
 }
 
-Config initial_config() {
-  Config cc = {WIFI_SSID, WIFI_PASS, MQTT_HOST, MQTT_PORT, TOPIC_STATE, TOPIC_SET};
-  uint8_t buff[16];
-  get_config_hashcode(buff);
-  memcpy(config.hashcode, buff, 16);
-  return cc;
-}
-
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  if (!_mqttClient.connected()) {
+    mqttReconnect();
   }
-  client.loop();
+  _mqttClient.loop();
 
-  check_button();
+  checkButton();
 
   // TODO Handle millis() rollover  
-  if (millis() - last_state > STATE_FREQ_MILLIS) {
-    update_state();
+  if (millis() - _lastState > STATE_FREQ_MILLIS) {
+    updateState();
   }
 }
